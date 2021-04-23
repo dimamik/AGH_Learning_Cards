18b46d0e0b85d7d1cd5d2b23967d3ca73f2b4bfe
dimamik <57190429+dimamik@users.noreply.github.com>
2021-04-23 20:28:20 +0200

    Changed db Schema

--- database/models/main_models.py
+++ database/models/main_models.py
@@ -4,19 +4,19 @@ from database.models.database_init import db
 
 class CardsCollection(BaseModel, db.Model):
     __tablename__ = 'CardsCollection'
-    cardID = db.Column(db.Integer, primary_key=True)
+    collectionID = db.Column(db.Integer, primary_key=True)
     collectionName = db.Column(db.String)
-    collectionDescription = db.Column(db.String)
+    collectionDescription = db.Column(db.JSON)
     holderID = db.Column(db.Integer, db.ForeignKey('User.userID'), nullable=False)
-    cardInfo = db.relationship('UserCardInfo', backref='cardsCollection', lazy=True)
-    singleCards = db.relationship('Card', backref='cardsCollection', lazy=True)
+    cardInfo = db.relationship('UserCollectionInfo', backref='cardsCollection', lazy=True)
+    cards = db.relationship('Card', backref='cardsCollection', lazy=True)
 
 
 class Card(BaseModel):
     __tablename__ = 'Card'
     cardID = db.Column(db.Integer, primary_key=True)
-    collectionID = db.Column(db.Integer, db.ForeignKey('CardsCollection.cardID'), nullable=False)
-    cardInside = db.Column(db.String)
+    collectionID = db.Column(db.Integer, db.ForeignKey('CardsCollection.collectionID'), nullable=False)
+    cardInside = db.Column(db.JSON)
 
 
 class User(BaseModel):
@@ -26,13 +26,21 @@ class User(BaseModel):
     userPasswordHash = db.Column(db.String)
     userInfo = db.Column(db.JSON)
     cards = db.relationship('CardsCollection', backref='user', lazy=True)
+    cardsWatched = db.relationship('CardWatched', backref='user', lazy=True)
 
 
-class UserCardInfo(BaseModel, db.Model):
-    __tablename__ = 'UserCardInfo'
+class UserCollectionInfo(BaseModel, db.Model):
+    __tablename__ = 'UserCollectionInfo'
     userID = db.Column(db.Integer, db.ForeignKey('User.userID'), primary_key=True, )
-    cardID = db.Column(db.Integer, db.ForeignKey('CardsCollection.cardID'), primary_key=True)
-    userCardInfo = db.Column(db.String)
+    collectionID = db.Column(db.Integer, db.ForeignKey('CardsCollection.collectionID'), primary_key=True)
+    userCardInfo = db.Column(db.JSON)
+
+
+class CardWatched(BaseModel):
+    __tablename__ = 'CardWatched'
+    cardWatchedID = db.Column(db.Integer, primary_key=True)
+    cardID = db.Column(db.Integer, db.ForeignKey('Card.cardID'), primary_key=True)
+    userID = db.Column(db.Integer, db.ForeignKey('User.userID'))
 
 
 def init_models():
